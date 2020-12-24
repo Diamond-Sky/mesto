24a2697719d662064740cc24356fcfc6c7bd6f5d
Dmitriy Bogatov <bogatoff.dm@gmail.com>
2020-12-24 21:45:46 +0300

    feat: JS popup and like-button, few media-query

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+
+//feat: Popup open/close on click
 let popupOpen = document.querySelector('.profile__edit-button');
 let popupOverlay = document.querySelector('.popup');
 let popupClose = popupOverlay.querySelector('.popup__close');
@@ -16,5 +18,29 @@ popupOverlay.addEventListener('click', (Event) => {
     }
 });
 
+//feat: Edit profile input
+let getForm = popupOverlay.querySelector('.popup__container');
+let firstName = popupOverlay.querySelector('.edit-form__firstname');
+let lastName = popupOverlay.querySelector('.edit-form__lastname');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 
+firstName.value = profileTitle.textContent;
+lastName.value = profileSubtitle.textContent;
+
+getForm.addEventListener('submit', (Event) => {
+    Event.preventDefault();
+    profileTitle.textContent = firstName.value;
+    profileSubtitle.textContent = lastName.value;
+    togglePopup();
+});
+
+//feat: likeButton Active
+let likeButton = document.querySelectorAll('.elements .elements__like');
+
+for (let i = 0; i<likeButton.length; i++) {
+likeButton[i].addEventListener('click', () => {
+    likeButton[i].classList.toggle('elements__like_active');
+})
+}
     
